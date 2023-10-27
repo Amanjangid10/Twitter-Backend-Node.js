e67c475fbe82ab7fa2381bd4e8ad43a39781fa0f
Amanjangid10 <mailme.aman.jangid@gmail.com>
2023-10-27 12:17:43 +0530

    mongo uri update

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const {URL}=require ("./server_config")
 
 const connect = async () => {
-    await mongoose.connect('mongodb+srv://mailmeaj10:Aman%401234@cluster0.2u0cs9c.mongodb.net/')
+    await mongoose.connect(URL)
 }
 
 module.exports = connect;
